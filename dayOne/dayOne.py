814f0f3da169db50ea4a9b17c018b227829ed22d
Bill Mansfield <manbil03@gmail.com>
2022-05-05 08:23:28 +1000

    fix/improve input data handling and implement functional style

--- dayOne/dayOne.py
+++ dayOne/dayOne.py
@@ -1,10 +1,20 @@
 # Count the number of times a depth measurement increases from the previous measurement
 # How many measurements are larger than the previous measurement?
-import dayOneInput
 
-increases = []
+# Get data
+with open('dayOne.in') as input:
+	data = [int(i) for i in input.read().strip().split("\n")]
 
-for firstN, secondN in zip(dayOneInput.nums, dayOneInput.nums[1:]):
-	increases.append(secondN > firstN)
+print data
 
-print(sum(increases))
+
+# Part 1
+def part1():
+	increases = []
+	for firstN, secondN in zip(data, data[1:]):
+		increases.append(secondN > firstN)
+
+	return sum(increases)
+
+
+print "Answer to part one:", part1()
