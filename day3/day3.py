b577f942348c83d643493e8b36e2636eced8b985
Bill Mansfield <manbil03@gmail.com>
2022-05-09 21:21:52 +1000

    fix/set up day 3 and improve file naming conventions

---
+++ day3/day3.py
@@ -0,0 +1,13 @@
+# --- Day 3: Binary Diagnostic ---
+
+# Get data
+with open('day3.in') as input:
+	data = [str(i) for i in input.read().strip().split("\n")]
+	print(data)
+
+# What do you get if you multiply your final horizontal position by your final depth?
+# Part 1
+def part1():
+
+
+print "Answer to part one:", part1()
