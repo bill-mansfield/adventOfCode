4cbae4c621c8fba64851fdcd7554c8e0bf1826ae
Bill Mansfield <manbil03@gmail.com>
2022-05-10 08:35:04 +1000

    feat/day3 part 1

--- day3/day3.py
+++ day3/day3.py
@@ -3,11 +3,37 @@
 # Get data
 with open('day3.in') as input:
 	data = [str(i) for i in input.read().strip().split("\n")]
-	print(data)
 
-# What do you get if you multiply your final horizontal position by your final depth?
+# Get power consumption
+# Power consumption = gamma rate * epsilon rate
+# Gamma rate is the most common value for each corresponding binary key
+# epsilon rate is the least common value for each corresponding binary key
+
 # Part 1
 def part1():
+	gamma = '' 
+	epsilon = ''
+
+	for digit in range(0, len(data[0])):
+		zeros = 0
+		ones = 0
+
+		for number in data:
+			if number[digit:digit + 1] == '0':
+				zeros += 1
+			else:
+				ones += 1
+
+		if int(zeros) > int(ones):
+			gamma += '0'
+			epsilon += '1'
+		else:
+			gamma += '1'
+			epsilon += '0'
+
+	return int(gamma, 2) * int(epsilon, 2)
+
+
 
 
 print "Answer to part one:", part1()
